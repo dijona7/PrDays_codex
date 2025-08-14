0cde2d1106dc6c71541bd259f9200e9c7cd5fcd1
dijona7 <66510115+dijona7@users.noreply.github.com>
2025-08-14 12:51:32 +0200

    Revamp UI with modern palette and navigation

--- app/src/main/java/com/algoritmika/prapp/ui/theme/Color.kt
+++ app/src/main/java/com/algoritmika/prapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.algoritmika.prapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BeigeBackground = Color(0xFFF4E9D7)
+val PrimaryBlue = Color(0xFF264F73)
+val AccentRed = Color(0xFFD84C3B)
+val AccentYellow = Color(0xFFE8B54B)
+val DarkText = Color(0xFF1E1E2C)
+val DarkBackground = Color(0xFF1A2230)
+val DarkCard = Color(0xFF263145)
